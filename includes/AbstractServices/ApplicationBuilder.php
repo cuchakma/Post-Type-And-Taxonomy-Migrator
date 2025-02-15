157164e0bafb1979b333f0de6fa01f7d7dd97f82
cupid chakma <greendaycu20@gmail.com>
2025-02-15 17:49:57 +0600

    update: Application Builder

--- includes/AbstractServices/ApplicationBuilder.php
+++ includes/AbstractServices/ApplicationBuilder.php
@@ -8,28 +8,40 @@ abstract class ApplicationBuilder implements AppicationBuilderInterface {
 
     public $services = [];
 
-    public function setServices( $hook, $serviceClassNamespace, $method = '', $properties = [], $priority = 10, $filterType = 'action' ) {
-        if ( $filterType == 'action' ) {
-            array_push( $this->services[$filterType], ['hook' => $hook, 'namespace' => $serviceClassNamespace, 'method' => $method, 'properties' => $properties, 'priority' => $priority] );
+    public function setServicesWithoutHook( $serviceClassNamespace ) {
+        if ( ! is_string( $serviceClassNamespace ) ) {
+            return;
+        }
+        array_push( $services, $serviceClassNamespace );
+        return $this;
+    }
+
+    public function setServicesOnHook( $hook, $serviceClassNamespace, $method = '', $properties = [], $priority = 10, $filterType = 'action' ) {
+        if ( ! isset( $this->services[$filterType] ) ) {
+            $this->services[$filterType] = [];
         }
 
-        if ( $filterType == 'filter' ) {
-            array_push( $this->services[$filterType], ['hook' => $hook, 'namespace' => $serviceClassNamespace, 'method' => $method, 'properties' => $properties, 'priority' => $priority] );
-        }
-        
+        $this->services[$filterType][] = [
+            'hook'       => $hook,
+            'namespace'  => $serviceClassNamespace,
+            'method'     => $method,
+            'properties' => $properties,
+            'priority'   => $priority
+        ];
+
         return $this;
     }
 
     public function build() {
         foreach ( $this->services as $hookType => $payload ) {
-            if ( $hookType == 'action' ) {
-                foreach ( $payload as $params ) {
-                    add_action( $params['hook'], [$params['namespace'], $params['method']], $params['priority'], ...$params['properties'] );
-                }
-            } else {
-                foreach ( $payload as $params ) {
-                    add_action( $params['hook'], [$params['namespace'], $params['method']], $params['priority'], ...$params['properties'] );
-                }
+            $actionType = $hookType == 'action' ? 'add_action' : 'add_filter';
+
+            foreach ( $payload as $params ) {
+                $actionType( $params['hook'], [$params['namespace'], $params['method']], $params['priority'], ...$params['properties'] );
+            }
+
+            if ( $actionType != 'action' && $actionType != 'filter' ) {
+                new $payload();
             }
         }
     }
--- includes/Interfaces/AppicationBuilderInterface.php
+++ includes/Interfaces/AppicationBuilderInterface.php
@@ -3,6 +3,34 @@
 namespace W3aliens\PostTypeAndTaxonomyMigrator\Interfaces;
 
 interface AppicationBuilderInterface {
-    public function setServices( $hook, $serviceClassNamespace, $method, $properties = [], $priority = 10, $filterType = 'action' );
+
+    /**
+     * Service Classes Without Hook
+     *
+     * @param string $serviceClassNamespace
+     * 
+     * @return object
+     */
+    public function setServicesWithoutHook( $serviceClassNamespace );
+
+    /**
+     * Attach Services With Hooks Included
+     *
+     * @param string $hook
+     * @param string $serviceClassNamespace
+     * @param string $method
+     * @param array $properties
+     * @param integer $priority
+     * @param string $filterType
+     * 
+     * @return object
+     */
+    public function setServicesOnHook( $hook, $serviceClassNamespace, $method, $properties = [], $priority = 10, $filterType = 'action' );
+
+    /**
+     * Build The Application When All Methods Are Set
+     *
+     * @return void
+     */
     public function build();
 }
